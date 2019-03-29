0393ff0bd1194057bd0fadc4e98cc1bb373684df
alumno <alumno@aula1-06>
2019-03-29 20:35:34 +0100

    last wey

--- src/buscaminas/Juego.java
+++ src/buscaminas/Juego.java
@@ -1,6 +1,8 @@
 
 package buscaminas;
 
+import java.util.Scanner;
+
 
 public class Juego {
     
@@ -8,11 +10,64 @@ public class Juego {
     private int numColumnas;
     private int numMInas;
     private Tablero tablero;
+
+    public Juego() {
+    
+       configurarJuego();
+       iniciarJuego();
+    
+    }
     
     
     public void configurarJuego(){
+        Scanner leer = new Scanner(System.in);
+        Juego j = new Juego();
         
+        System.out.println("Dime las filas y columnas del tablero");
+        numFilas = leer.nextInt();
+        numColumnas = leer.nextInt();
+        System.out.println("Dime la cantidad de minas con las que quieres jugar");
+        numMInas = leer.nextInt();          
         
+       
     }
+    
+    
+    public void iniciarJuego(){
+        
+        Tablero t = new Tablero(numMInas, numFilas, numColumnas);
+        t.insertarMinas(numMInas);
+        t.calcularTablero();
+    }
+    
+    public void elegirOperacion(){
+        
+        int opc = 0;
+        
+        while(true){
+            Menu.mostraMenu();
+            switch(opc){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+    }
+
+    public void setNumColumnas(int numColumnas) {
+        this.numColumnas = numColumnas;
+    }
+
+    public void setNumMInas(int numMInas) {
+        this.numMInas = numMInas;
+    }
+
+    public void setNumFilas(int numFilas) {
+        this.numFilas = numFilas;
+    }
+    
     
 }
